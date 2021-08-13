19be5a27c8d6f368dc3ebc1d65759a43e085b7b0
Prabhnoor Singh <86031490+Prabhnoor-2001@users.noreply.github.com>
2021-08-13 15:34:07 +1200

    stretch

--- addNames.js
+++ addNames.js
@@ -30,3 +30,42 @@ function addName(name,img){
   }
 })
 }
+//on route
+router.get('/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+  const filePath = path.join(__dirname, 'data.json')
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.log('err')
+    }
+    try {
+      const newData = JSON.parse(data)
+      const vd = newData.puppies.find(el => id === el.id)
+      // console.log(vd);
+      res.render('edit', vd)// take it to the page that has the edit html
+    } catch (parseError) {
+      console.log('parseError')
+    }
+  })
+})
+router.post('/:id/edit', (req, res) => {
+  // const id = Number(req.params.id)
+  // const body = { ...req.body }
+  //let updatedPups = {}
+
+  const filePath = path.join(__dirname, 'data.json')
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.log('err')
+    }
+    try {
+      // console.log(data)
+        addName(req.body.Name,req.body.Image)// changes the json file and adds a person object with name and image in peopleNames
+        res.redirect('/')
+      
+    } catch (parseError) {
+      console.log('parseError')
+    }
+  })
+})
