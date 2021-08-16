d69482b56308165ce8f29b0debe83acf56da0547
Ali Sultani <26952644+AliakbarSu@users.noreply.github.com>
2021-08-16 17:01:29 +1200

    Fix randomizer

--- random.js
+++ random.js
@@ -1,15 +1,21 @@
 module.exports = {assignGroups, assignGroupNames}
 
 
-function assignGroups(peoplesNamesArray){
+function assignGroups(peoplesNamesArra){
   //console.log(peoplesNamesArray);
+  const peoplesNamesArray = shuffleArray(peoplesNamesArra)
+  console.log(peoplesNamesArray);
   const teams = [[],[],[],[]]// push peoples names into array
   const length = peoplesNamesArray.length
   const remainder = length %4
   for(let i= 0; i<length; i++){
+
+
     let num = Math.floor(Math.random() * teams.length); 
        if(teams[num].length < 4){
         teams[num].push(peoplesNamesArray[i])    }
+
+
     else {
       if(i>= length - remainder){   
         if(teams[num].length !== 5){
@@ -39,6 +45,14 @@ function assignGroups(peoplesNamesArray){
 //HAVE AN ARRAY THAT HAS RANDOMLY PLACED INDEXES AND GO THROUGH IT NORMALLY AND RUN THE SAME CODE
  
 }
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array
+}
+
 
 function assignGroupNames(jsonTeam, teamsArray){// jsonTeam is "team" key 
    const returnObj = []
