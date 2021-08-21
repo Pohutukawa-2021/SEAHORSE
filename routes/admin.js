2724dd7ced809ac21d8e5d1e53a2dfdcf3f36d68
unknown <eng.ahmed.anwar@gmail.com>
2021-08-21 17:04:02 +1200

    improve UI

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const router = express.Router()
-
+const db = require('../db/index')
 module.exports = router
 
-router.get('/select', (req, res) => {
-  res.render('select')
+// GET /admin/select
+router.get('/select', async (req, res) => {
+  try {
+    const people = await db.getPeople()
+    const themes = await db.getThemes()
+    const viewData = {
+      people,
+      themes
+    }
+    res.render('select', viewData)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('bad error')
+  }
+})
+
+// GET /admin/add/theme
+router.get('/theme/add', (req, res) => {
+  res.render('addTheme')
+})
+
+// POST /admin/theme/add
+router.post('/theme/add', (req, res) => {
+  res.redirect('/admin/select')
 })
